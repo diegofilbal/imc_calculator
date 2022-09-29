b81af82a0b929cd66e3ed252acc9a77c3449ba23
diegofilbal <diegofilbal@ufrn.edu.br>
2022-09-28 23:54:42 -0300

    Implementa calculador de IMC versão 2

---
+++ imc_calculator/lib/ui/result.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  String _imagem;
+  String _texto;
+
+  Result(this._imagem, this._texto);
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Resultado"), backgroundColor: Colors.blueAccent),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            _imagem,
+            fit: BoxFit.cover,
+            height: 300
+          ),
+          Center(
+            child: Text(
+              _texto,
+              style: TextStyle(fontSize: 22, fontStyle: FontStyle.italic)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
